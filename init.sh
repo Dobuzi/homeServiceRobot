7989f9b70500408d2cf8cbbee955dedd312ddfc5
Dobuzi <kimjw7981@gmail.com>
2021-05-17 14:11:12 +0000

    edit init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 git config --global user.email "kimjw7981@gmail.com"
 git config --global user.name "Dobuzi"
+git config --global push.default simple
+pip install --upgrade pip
 pip install rospkg
 pip3 install rospkg
