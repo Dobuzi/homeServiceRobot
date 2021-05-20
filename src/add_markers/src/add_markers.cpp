e5da7e17b01b5e70282c3bdad1eff2a68366bf25
Dobuzi <kimjw7981@gmail.com>
2021-05-20 07:04:15 +0000

    implement the pick up and drop off algorithm

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,9 +1,14 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
+#include <math.h>
 
-visualization_msgs::Marker setMarker(visualization_msgs::Marker marker, int position[3], double orientation[4], double scale[3], float color[3], double alpha);
-void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg);
+visualization_msgs::Marker setMarker(visualization_msgs::Marker marker, double position[3], double orientation[4], double scale[3], float color[3], double alpha);
+void getPosition(const nav_msgs::Odometry::ConstPtr& msg);
+double getDistance(double goal[2], double position[2]);
+bool isReached(double distance, double threshold);
+
+double robot_position[2] = {0, 0};
 
 int main( int argc, char** argv )
 {
@@ -12,16 +17,26 @@ int main( int argc, char** argv )
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
   // Subscribe odom
-  ros::Subscriber odom_sub = n.subscribe("odom", 1000, chatterCallback);
+  ros::Subscriber odom_sub = n.subscribe("/odom", 10, getPosition);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
   int step = 0;
+  // 0 : drive to pickup zone
+  // 1 : picked object (hide and wait 5 seconds)
+  // 2 : drive to drop off zone
+  // 3 : drop off the object
 
-  int position[3] = {1, 1, 0};
+  double pick_up_zone[2] = {1, 1};
+  double drop_off_zone[2] = {2, 2};  
+  double threshold = 0.3;
+  double distance = 99999;
+  int wait = 0;
+
+  double goal_position[3] = {1, 1, 0};
   double orientation[4] = {0.0, 0.0, 0.0, 1.0};
-  double scale[3] = {0.1, 0.1, 0.1};
+  double scale[3] = {0.3, 0.3, 0.3};
   float color[3] = {0.0f, 1.0f, 0.0f};
   double alpha = 1.0;
 
@@ -39,40 +54,62 @@ int main( int argc, char** argv )
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
     marker.type = shape;
-
     
     switch (step) {
       case 0:
-        position[0] = 1;
-        position[1] = 1;
+        goal_position[0] = pick_up_zone[0];
+        goal_position[1] = pick_up_zone[1];
         marker.action = visualization_msgs::Marker::ADD;
-        step = 1;
-        ROS_INFO("Set the marker at the pickup zone");
+        distance = getDistance(pick_up_zone, robot_position);
+        if (isReached(distance, threshold)) {
+          step = 1;
+        } else {
+          ROS_INFO_STREAM("Drive to the pickup zone ; distance: " << distance);
+        }
         break;
       case 1:
-        sleep(5);
         marker.action = visualization_msgs::Marker::DELETE;
-        step = 2;
-        ROS_INFO("Hide the marker after pickup");
+        if (wait >= 5) {
+          wait = 0;
+          step = 2;
+          ROS_INFO("Picked up the object");
+        } else {
+          ROS_INFO_STREAM("Picking up the object " << wait+1 << "s/5s");
+          sleep(1);
+          wait++;
+        }
         break;
       case 2:
-        sleep(5);
-        position[0] = 2;
-        position[1] = 2;
-        marker.action = visualization_msgs::Marker::ADD;
-        step = 3;
-        ROS_INFO("Set the marker at the drop off zone");
+        goal_position[0] = drop_off_zone[0];
+        goal_position[1] = drop_off_zone[1];
+        marker.action = visualization_msgs::Marker::DELETE;
+        distance = getDistance(drop_off_zone, robot_position);
+        if (isReached(distance, threshold)) {
+          step = 3;
+        } else {
+          ROS_INFO_STREAM("Drive to the drop-off zone ; distance: " << distance);
+        }
         break;
       case 3:
-        sleep(5);
-        marker.action = visualization_msgs::Marker::DELETE;
-        step = 0;
-        ROS_INFO("Hide the marker after drop off");
+        marker.action = visualization_msgs::Marker::ADD;
+        ROS_INFO("Drop off the object");
+        ROS_INFO("Mission Complete");
+        step = 4;
         break;
-        
+      case 4:
+        if (wait >= 5) {
+          wait = 0;
+          step = 0;
+          ROS_INFO("Go To the first step");
+          marker.action = visualization_msgs::Marker::DELETE;
+        } else {
+          ROS_INFO_STREAM("Wait for next cycle " << wait+1 << "s/5s");
+          sleep(1);
+          wait++;
+        }
     }
 
-    marker = setMarker(marker, position, orientation, scale, color, alpha);
+    marker = setMarker(marker, goal_position, orientation, scale, color, alpha);
 
     marker.lifetime = ros::Duration();
 
@@ -87,7 +124,7 @@ int main( int argc, char** argv )
       sleep(1);
     }
     marker_pub.publish(marker);
-
+    ros::spinOnce();
     r.sleep();
   }
   
@@ -95,7 +132,7 @@ int main( int argc, char** argv )
   return 0;
 }
 
-visualization_msgs::Marker setMarker(visualization_msgs::Marker marker, int position[3], double orientation[4], double scale[3], float color[3], double alpha) {
+visualization_msgs::Marker setMarker(visualization_msgs::Marker marker, double position[3], double orientation[4], double scale[3], float color[3], double alpha) {
   marker.pose.position.x = position[0];
   marker.pose.position.y = position[1];
   marker.pose.position.z = position[2];
@@ -117,10 +154,20 @@ visualization_msgs::Marker setMarker(visualization_msgs::Marker marker, int posi
   return marker;
 }
 
-void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void getPosition(const nav_msgs::Odometry::ConstPtr& msg)
 {
-  ROS_INFO("Seq: [%d]", msg->header.seq);
-  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
-  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-  ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+  robot_position[0] = msg->pose.pose.position.x;
+  robot_position[1] = msg->pose.pose.position.y;
+}
+
+double getDistance(double goal[2], double position[2])
+{
+  double distance;
+  distance = sqrt(pow(goal[0] - position[0], 2) + pow(goal[1] - position[1], 2));
+  return distance;
+}
+
+bool isReached(double distance , double threshold)
+{
+  return distance < threshold;
 }
