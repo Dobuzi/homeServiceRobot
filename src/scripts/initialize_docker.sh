d7757031845a2cdf544788054056b5d56c0dc14f
Dobuzi <kimjw7981@gmail.com>
2021-05-31 14:14:07 +0900

    edit scripts

--- src/scripts/initialize_docker.sh
+++ src/scripts/initialize_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-apt-get update && apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y
 apt-get install xterm python3-pip -y
 pip install --upgrade pip
 pip3 install --upgrade pip
