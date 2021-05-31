1818a6eb25aad5704dd0ff12face87f1d069b9d3
Dobuzi <kimjw7981@gmail.com>
2021-05-31 14:48:08 +0900

    edit scripts

--- src/scripts/initialize.sh
+++ src/scripts/initialize.sh
@@ -6,15 +6,14 @@ sudo python -m pip install --upgrade pip
 sudo python3 -m pip3 install --upgrade pip
 sudo python -m pip install rospkg
 sudo python3 -m pip3 install rospkg
-source /opt/ros/kinetic/setup.bash
-catkin_make
-source devel/setup.bash
 cd src/
 git clone https://github.com/ros-perception/slam_gmapping.git  
 git clone https://github.com/turtlebot/turtlebot.git  
 git clone https://github.com/turtlebot/turtlebot_interactions.git  
 git clone https://github.com/turtlebot/turtlebot_simulator.git
 cd ../
+source /opt/ros/kinetic/setup.bash
+catkin_make
 source devel/setup.bash
 catkin_make
 source devel/setup.bash
@@ -22,5 +21,6 @@ rosdep -i install gmapping -y
 rosdep -i install turtlebot_teleop -y
 rosdep -i install turtlebot_rviz_launchers -y
 rosdep -i install turtlebot_gazebo -y
+source devel/setup.bash
 catkin_make
 source devel/setup.bash
