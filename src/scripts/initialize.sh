455429ddc1790bdafb695a9c67b957c23c46359a
Dobuzi <kimjw7981@gmail.com>
2021-06-04 13:42:03 +0900

    edit initialize

--- src/scripts/initialize.sh
+++ src/scripts/initialize.sh
@@ -2,15 +2,12 @@
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update -y && apt-get upgrade -y
 sudo apt-get install xterm python3-pip -y
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install rospkg
 cd src/
 git clone https://github.com/ros-perception/slam_gmapping.git  
 git clone https://github.com/turtlebot/turtlebot.git  
 git clone https://github.com/turtlebot/turtlebot_interactions.git  
 git clone https://github.com/turtlebot/turtlebot_simulator.git
 cd ../
-. /opt/ros/kinetic/setup.bash
 catkin_make
 chmod +x devel/setup.bash
 . devel/setup.bash
