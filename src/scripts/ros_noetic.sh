c5383ef715e63af5f05b24963f83240e627f13f3
Dobuzi <kimjw7981@gmail.com>
2021-06-04 13:25:48 +0900

    edit mode of ros_xxx

--- src/scripts/ros_noetic.sh
+++ src/scripts/ros_noetic.sh
@@ -2,8 +2,8 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-desktop-full -y
 source /opt/ros/noetic/setup.bash
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 sudo rosdep init
 rosdep update
